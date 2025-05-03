7e32d2f0e5e51461cfffc2bc6b8609c9f1bf6006
FransergioDev <fransergio.dev@gmail.com>
2025-05-03 15:44:54 -0300

    Configurações de validação da autenticação

--- Program.cs
+++ Program.cs
@@ -1,14 +1,42 @@
 //using Microsoft.AspNetCore.Mvc;
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using TarefasBackEnd.Config;
 using TarefasBackEnd.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
 builder.Services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("DBTasks"));
 builder.Services.AddTransient<ITarefaRepository, TarefaRepository>();
+builder.Services.AddTransient<IUsuarioRepository, UsuarioRepository>();
 
 builder.Services.AddControllers();
+
+var appSettings = builder.Configuration.GetSection("AppSettings").Get<AppSettings>();
+if (appSettings != null)
+{
+    var secretKey = appSettings.getSecretKeyBytes();
+
+    builder.Services.AddAuthentication(option =>
+    {
+        option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(secretKey),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });   
+}
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -26,6 +54,8 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/test", () => new { message = "Hello World!" });
 
+app.UseAuthentication();
+app.UseAuthorization(); 
 app.MapControllers();
 
 app.Run();
